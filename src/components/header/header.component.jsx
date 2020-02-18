8f576670551d5decf5d69d05d8cd9f0df4f11c69
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-02-18 23:24:09 +0200

    fixed link to sign in page not working propperly

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,7 +20,7 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="./signin">
+        <Link className="option" to="/signin">
           SIGN IN
         </Link>
       )}
