e803339c7bda807def644625923707010e9c000a
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-02-18 16:18:03 +0200

    created sing-in component and custom button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 
+import SingInAndSignUpPage from "./pages/sign-in-and-sign-up/sing-in-and-sign-up.component";
+
 function App() {
   return (
     <div>
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SingInAndSignUpPage} />
       </Switch>
     </div>
   );
